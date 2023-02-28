41d89dc3844377e06738ed0287fe24b32434a978
dexi-1bit <dexi@1bit.com.ph>
2023-02-28 15:19:00 +0800

    appbrand changes side menu logo

--- src/layouts/components/AppBrand/index.tsx
+++ src/layouts/components/AppBrand/index.tsx
@@ -1,21 +1,37 @@
+// ** MUI Imports
+import { useTheme } from '@mui/material/styles'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 
 // ** Hook Import
 import { useSettings } from 'src/@core/hooks/useSettings'
 
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
 const AppBrand = () => {
 
     // ** Hook
     const { settings, saveSettings } = useSettings()
-    
+    const { navCollapsed } = settings
+    const theme = useTheme()
+
     return (
       <Box
-        
         sx={{ display: 'flex', alignItems: 'center' }}>
-        <Typography variant='h6' sx={{ ml: 2 }}>
-          Main Admin
-        </Typography>
+        
+          {
+            navCollapsed 
+            ? 
+              <Box 
+              onClick={() => saveSettings({ ...settings, navCollapsed: !navCollapsed })}
+                sx={{ cursor:'pointer', ml: 6 , display: 'flex', color: theme => theme.palette.primary.main }}>
+                <Icon icon='mdi:menu' fontSize={20} />
+              </Box>
+            : 
+              <Typography variant='h6' sx={{ ml: 2 }}>MAIN ADMIN</Typography>
+          }
+        
       </Box>
     )
   }
