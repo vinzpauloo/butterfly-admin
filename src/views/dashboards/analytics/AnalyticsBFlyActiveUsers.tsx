583546f006315c644c18358e286b13a2661c9f58
dexi-1bit <dexi@1bit.com.ph>
2023-02-28 15:18:04 +0800

    create anyatics views

---
+++ src/views/dashboards/analytics/AnalyticsBFlyActiveUsers.tsx
@@ -0,0 +1,46 @@
+// ** MUI Imports
+import Card from '@mui/material/Card'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import CardContent from '@mui/material/CardContent'
+import { styled, useTheme } from '@mui/material/styles'
+
+// Styled component for the triangle shaped background image
+const TriangleImg = styled('img')(({ theme }) => ({
+  right: 0,
+  bottom: 0,
+  height: 170,
+  position: 'absolute',
+  ...(theme.direction === 'rtl' ? { transform: 'scaleX(-1)' } : {})
+}))
+
+// Styled component for the trophy image
+const TrophyImg = styled('img')({
+  right: 36,
+  bottom: 20,
+  height: 98,
+  position: 'absolute'
+})
+
+const AnalyticsBFlyCC = () => {
+  // ** Hook
+  const theme = useTheme()
+
+  const imageSrc = theme.palette.mode === 'light' ? 'triangle-light.png' : 'triangle-dark.png'
+
+  return (
+    <Card sx={{ position: 'relative' }}>
+      <CardContent>
+        <Typography variant='h4' sx={{ my: 0, color: 'primary.main' }}>
+          500
+        </Typography>
+        <Typography variant='body2' sx={{ letterSpacing: '0.25px' }}>
+          Total Active Users
+        </Typography>
+        <TriangleImg alt='triangle background' src={`/images/misc/${imageSrc}`} />
+      </CardContent>
+    </Card>
+  )
+}
+
+export default AnalyticsBFlyCC
