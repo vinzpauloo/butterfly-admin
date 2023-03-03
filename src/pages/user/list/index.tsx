ed01c624379cb6f38cdd02b163a53ce69d9490d0
VincentPaul <oo.vincentpaul@1bit.com.ph>
2023-03-03 19:45:05 +0800

    Componetized Operator and SuperAgentEditModals, updated info and function of edit button

--- src/pages/user/list/index.tsx
+++ src/pages/user/list/index.tsx
@@ -10,12 +10,6 @@ import {
   IconButton,
   Typography,
   Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  TextField,
-  Checkbox,
 } from '@mui/material';
 import UnfoldMoreTwoToneIcon from '@mui/icons-material/UnfoldMoreTwoTone';
 import ArrowBackIos from '@mui/icons-material/ArrowBackIos';
@@ -23,218 +17,8 @@ import ArrowForwardIos from '@mui/icons-material/ArrowForwardIos';
 import { styled } from '@mui/system';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 
-//Forms
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-
-interface FormValues {
-  operator: boolean;
-  supervisor: boolean;
-  username: string;
-  password: string;
-  confirmPassword: string;
-  mobileNo: string;
-  emailAddress: string;
-  notes: string;
-}
-
-const schema = yup.object().shape({
-  username: yup.string().required(),
-  password: yup.string().required(),
-  confirmPassword: yup
-    .string()
-    .oneOf([yup.ref('password'), null], 'Passwords must match')
-    .required(),
-  mobileNo: yup.string().required(),
-  emailAddress: yup.string().email().required(),
-});
-
-interface FormModalProps {
-  isOpen: boolean;
-  onClose: () => void;
-}
-
-const FormModal: React.FC<FormModalProps> = ({ isOpen, onClose }) => {
-  const [formValue, setFormValue] = useState<FormValues>({
-    operator: false,
-    supervisor: false,
-    username: '',
-    password: '',
-    confirmPassword: '',
-    mobileNo: '',
-    emailAddress: '',
-    notes: '',
-  });
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormValues>({
-    resolver: yupResolver(schema),
-  });
-
-  const handleFormSubmit = () => {
-    alert(JSON.stringify(formValue));
-    onClose();
-  };
-
-  const handleFormInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, checked } = event.target;
-
-    if (name === 'operator' && checked) {
-      setFormValue((prevState) => ({
-        ...prevState,
-        operator: true,
-        supervisor: false,
-      }));
-    } else if (name === 'supervisor' && checked) {
-      setFormValue((prevState) => ({
-        ...prevState,
-        operator: false,
-        supervisor: true,
-      }));
-    } else {
-      setFormValue((prevState) => ({
-        ...prevState,
-        [name]:
-          event.target.type === 'checkbox' ? event.target.checked : event.target.value,
-      }));
-    }
-  };
-
-  return (
-    <Dialog open={isOpen} onClose={onClose}>
-      <div style={{display: 'flex', backgroundColor: '#A459D1'}}>
-        <DialogTitle sx={{color: 'white'}}>Operator</DialogTitle>
-        <Checkbox
-          name="operator"
-          checked={formValue.operator}
-          onChange={handleFormInputChange}
-          inputProps={{ 'aria-label': 'primary checkbox' }}
-          sx={{color: 'white'}}
-          color="default"
-        />
-        <DialogTitle sx={{color: 'white'}}>Supervisor</DialogTitle>
-        <Checkbox
-          name="supervisor"
-          checked={formValue.supervisor}
-          onChange={handleFormInputChange}
-          inputProps={{ 'aria-label': 'primary checkbox' }}
-          sx={{color: 'white'}}
-          color="default"
-        />
-      </div>
-      <DialogContent>
-        <form onSubmit={handleSubmit(handleFormSubmit)}>
-          <Typography>Username</Typography>
-          <TextField
-            label="Entire Desired Username"
-            variant="outlined"
-            fullWidth
-            {...register('username')}
-            error={!!errors.username}
-            helperText={errors.username?.message}
-            value={formValue.username}
-            onChange={handleFormInputChange}
-            name="username"
-          />
-          <Box style={{display: 'flex', gap: 20, marginTop: 20, marginBottom: 20}}>
-            <Box sx={{width: '50%'}}>
-              <Typography>Password</Typography>
-              <TextField
-                label="Enter Password"
-                variant="outlined"
-                fullWidth
-                type="password"
-                {...register('password')}
-                error={!!errors.password}
-                helperText={errors.password?.message}
-                value={formValue.password}
-                onChange={handleFormInputChange}
-                name="password"
-              />
-            </Box>
-            <Box sx={{width: '50%'}}>
-              <Typography>Re-enter Password</Typography>
-              <TextField
-                label="Re-enter Password"
-                variant="outlined"
-                fullWidth
-                type="password"
-                {...register('confirmPassword')}
-                error={!!errors.confirmPassword}
-                helperText={errors.confirmPassword?.message}
-                value={formValue.confirmPassword}
-                onChange={handleFormInputChange}
-                name="confirmPassword"
-              />
-            </Box>
-          </Box>
-          <Box style={{display: 'flex', gap: 20, marginBottom: 20}}>
-            <Box sx={{width: '50%'}}>
-              <Typography>Mobile No.</Typography>
-              <TextField
-                label="Mobile No."
-                variant="outlined"
-                fullWidth
-                {...register('mobileNo')}
-                error={!!errors.mobileNo}
-                helperText={errors.mobileNo?.message}
-                value={formValue.mobileNo}
-                onChange={handleFormInputChange}
-                name="mobileNo"
-              />
-            </Box>
-            <Box sx={{width: '50%'}}>
-              <Typography>Email Address</Typography>
-              <TextField
-                label="Email Address"
-                variant="outlined"
-                fullWidth
-                {...register('emailAddress')}
-                error={!!errors.emailAddress}
-                helperText={errors.emailAddress?.message}
-                value={formValue.emailAddress}
-                onChange={handleFormInputChange}
-                name="emailAddress"
-              />
-            </Box>
-          </Box>
-          <Typography>Notes (Optional)</Typography>
-          <TextField
-            label="Notes"
-            variant="outlined"
-            fullWidth
-            multiline
-            rows={4}
-            {...register('notes')}
-            value={formValue.notes}
-            onChange={handleFormInputChange}
-            name="notes"
-          />
-          <DialogActions>
-            <Button onClick={onClose} sx={{ backgroundColor: '#98A9BC', color: 'white', width: '200px', '&:hover': {
-                backgroundColor: '#7899ac',
-              }, }}>
-              <Typography sx={{color: 'white', textTransform: 'uppercase','&:hover': {transform: 'scale(1.1)',
-                  transition: 'transform 0.2s ease-in-out',
-                },}}>Cancel</Typography>
-            </Button>
-            <Button type="submit" sx={{ backgroundColor: '#9747FF', color: 'white', width: '200px', '&:hover': {
-                backgroundColor: '#9747FF',
-              }, }}>
-              <Typography sx={{color: 'white', textTransform: 'uppercase','&:hover': {transform: 'scale(1.1)',
-                  transition: 'transform 0.2s ease-in-out',
-                },}}>Update</Typography>
-            </Button>
-          </DialogActions>
-        </form>
-      </DialogContent>
-    </Dialog>
-  );
-};
+import OperatorEditModal from "../components/OperatorEditModal";
+import SuperAgentEditModal from "../components/SuperAgentEditModal";
 
 const ToggleButton = () => {
   const [isOn, setIsOn] = useState(false);
@@ -300,7 +84,8 @@ const ToggleButton = () => {
         <button style={{background: 'transparent', border: 'none'}} onClick={handleModalOpen}>
           <EditOutlinedIcon sx={{color: '#98A9BC', fontSize: 30}}/>
         </button>
-        <FormModal isOpen={isModalOpen} onClose={handleModalClose} />
+        {/*<OperatorEditModal isOpen={isModalOpen} onClose={handleModalClose} />*/}
+        <SuperAgentEditModal isOpen={isModalOpen} onClose={handleModalClose} />
       </Box>
     </Box>
   );
