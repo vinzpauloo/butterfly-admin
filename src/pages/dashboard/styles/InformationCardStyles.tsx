56302f8de63fc4ea362fe68babfa3891d13cb18d
VincentPaul <oo.vincentpaul@1bit.com.ph>
2023-03-13 17:08:01 +0800

    Created dashboard

---
+++ src/pages/dashboard/styles/InformationCardStyles.tsx
@@ -0,0 +1,34 @@
+const styles = {
+  box: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  avatar: {
+    mr: 3,
+    boxShadow: 3,
+    width: 44,
+    height: 44,
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  card: {
+    width: {
+      xs: '100%',
+      md: '100%',
+      lg: '25%'
+    }
+  },
+  subHead: {
+    fontWeight: 600,
+    color: 'text.primary'
+  },
+  title: {
+    mb: 2.5,
+    lineHeight: '2rem !important',
+    letterSpacing: '0.15px !important'
+  }
+}
+
+export default styles;
