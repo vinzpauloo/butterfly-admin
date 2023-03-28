65b8b27219503442d563e8b956741d450abfd6d3
dexi-1bit <dexi@1bit.com.ph>
2023-03-28 15:41:29 +0800

    Clean up unused

--- src/pages/studio/video-list/index.tsx
+++ src/pages/studio/video-list/index.tsx
@@ -1,30 +1,5 @@
 // ** React Imports
-import React,{ useState } from 'react'
-
-// ** MUI Imports
-import Box from '@mui/material/Box'
-import Card from '@mui/material/Card'
-import Typography from '@mui/material/Typography'
-import CardHeader from '@mui/material/CardHeader'
-import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid'
-
-// ** Custom Components
-import CustomAvatar from 'src/@core/components/mui/avatar'
-import SearchToolbar from '@/pages/studio/shared-component/SearchToolbar'
-import VideoApprovalDialog from '@/pages/studio/shared-component/VideoApprovalDialog'
-
-// ** Types Imports
-import { ThemeColor } from 'src/@core/layouts/types'
-import { DataVideosGridRowType } from '@/pages/studio/types/types'
-
-// ** Utils Import
-import { getInitials } from 'src/@core/utils/get-initials'
-
-// ** Data Import
-import { rows } from '@/data/dummyVideosUploaded'
-
-// ** API Imports
-import VideoService from '@/services/api/VideoService'
+import React from 'react'
 
 import TableVideos from './table/TableVideos'
 
