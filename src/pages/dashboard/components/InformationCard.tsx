a87de8526a628789e78df7346472f755b026f78d
VincentPaul <oo.vincentpaul@1bit.com.ph>
2023-03-29 18:23:28 +0800

    Deleted InformationCard styles

--- src/pages/dashboard/components/InformationCard.tsx
+++ src/pages/dashboard/components/InformationCard.tsx
@@ -11,7 +11,6 @@ import Image from 'next/image'
 import { ThemeColor } from 'src/@core/layouts/types'
 import OptionsMenu from '@/@core/components/option-menu'
 import CustomAvatar from 'src/@core/components/mui/avatar'
-import styles from '../styles/InformationCardStyles'
 
 interface DataType {
   stats: string
@@ -117,6 +116,39 @@ const InformationCard = () => {
       </CardContent>
     </Card>
   )
+}
+
+const styles = {
+  box: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  avatar: {
+    mr: 3,
+    boxShadow: 3,
+    width: 44,
+    height: 44,
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  card: {
+    width: {
+      xs: '100%',
+      md: '100%',
+      lg: '50%'
+    }
+  },
+  subHead: {
+    fontWeight: 600,
+    color: 'text.primary'
+  },
+  title: {
+    mb: 2.5,
+    lineHeight: '2rem !important',
+    letterSpacing: '0.15px !important'
+  }
 }
 
 export default InformationCard
