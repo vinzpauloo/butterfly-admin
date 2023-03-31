9a9083b2f5bae1f7a407dd67bd251dc50171963e
Mark <mark@1bit.com.ph>
2023-03-31 17:18:20 +0800

    add search and modal

--- src/services/api/Workgroup.ts
+++ src/services/api/Workgroup.ts
@@ -1,15 +1,32 @@
 import request from '../../lib/request'
 import authConfig from 'src/configs/auth'
 
-interface IWorkgroup {
+interface IGetWorkgroup {
   page: number
   paginate: number
+}
+interface IGetSearchWorkgroup {
+  page: number
+  search_by: string
+  search_value?: string
+  select: string
+  navbar?: string
+  template_id?: string
+}
+
+interface IPostWorkgroup {
+  title: string
+  navbar: string
+  template_id: string
+  single?: string
+  multiple?: string[]
+  all: string[]
 }
 
 const WorkgroupService = () => {
   const accessToken = window.localStorage.getItem(authConfig.storageTokenKeyName)
 
-  const getWorkgroup = (params: IWorkgroup) => {
+  const getWorkgroup = (params: IGetWorkgroup) => {
     return request({
       headers: {
         'X-Authorization': 'postman|1',
@@ -22,7 +39,33 @@ const WorkgroupService = () => {
     })
   }
 
-  return { getWorkgroup }
+  const postWorkgroup = (params: IPostWorkgroup) => {
+    return request({
+      headers: {
+        'X-Authorization': 'postman|1',
+        'ngrok-skip-browser-warning': '69420', // only for dev
+        Authorization: `Bearer ${accessToken}`
+      },
+      url: '/workgroups',
+      method: 'POST',
+      params: params
+    })
+  }
+
+  const getSearchWorkgroups = (params: IGetSearchWorkgroup) => {
+    return request({
+      headers: {
+        'X-Authorization': 'postman|1',
+        'ngrok-skip-browser-warning': '69420', // only for dev
+        Authorization: `Bearer ${accessToken}`
+      },
+      url: '/workgroups',
+      method: 'GET',
+      params: params
+    })
+  }
+
+  return { getWorkgroup, postWorkgroup, getSearchWorkgroups }
 }
 
 export default WorkgroupService
