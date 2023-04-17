89d2fc4b8ffb2cba759d56ad7cb1c44dc7c95a17
dexi-1bit <dexi@1bit.com.ph>
2023-04-17 19:50:03 +0800

    Create TableNewsFeedApproval

---
+++ src/pages/studio/newsfeed/views/TableNewsFeedApproval.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const TableNewsFeedApproval = (props: Props) => {
+  return (
+    <div>TableNewsFeedApproval</div>
+  )
+}
+
+export default TableNewsFeedApproval
