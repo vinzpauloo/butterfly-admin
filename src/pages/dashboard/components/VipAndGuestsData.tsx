3142e6a8d72e420d73935f34e3ed4a33ff9565a6
VincentPaul <oo.vincentpaul@1bit.com.ph>
2023-04-26 15:36:50 +0800

    API Integration for VIP and Guests Chart, UI - Added Date Picker, mobile responsiveness, completed the chart API for Video contents

--- src/pages/dashboard/components/VipAndGuestsData.tsx
+++ src/pages/dashboard/components/VipAndGuestsData.tsx
@@ -1,12 +1,21 @@
 // ** React Imports
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DatePicker from 'react-datepicker'
 
 // ** MUI Imports
-import { Card, CardHeader, CardContent } from '@mui/material'
+import { Box, Card, CardHeader, CardContent } from '@mui/material'
 
 // ** Third Party Imports
 import { Bar } from 'react-chartjs-2'
 import { ChartData, ChartOptions } from 'chart.js'
+
+// ** Project/Other Imports
+import DatePickerWrapper from '@/@core/styles/libs/react-datepicker'
+import PickersComponent from '@/layouts/components/shared-components/Picker/CustomPickerInput'
+import { DashboardService } from '@/services/api/DashboardService'
+
+// ** TanStack Query Imports
+import { useQuery } from '@tanstack/react-query'
 
 interface VerticalBarProps {
   info: string
@@ -16,8 +25,74 @@ interface VerticalBarProps {
   legendColor: string
 }
 
+const generateChartData = (startDate: Date, endDate: Date, warning: string, info: string) => {
+  const monthsBetween = Math.abs(
+    startDate.getMonth() - endDate.getMonth() + 12 * (startDate.getFullYear() - endDate.getFullYear())
+  )
+
+  const labels = Array.from({ length: monthsBetween + 1 }, (_, i) => {
+    const currentMonth = new Date(startDate).setMonth(startDate.getMonth() + i)
+    const month = new Date(currentMonth).toLocaleString('default', { month: 'long' })
+    const year = new Date(currentMonth).getFullYear()
+
+    return `${month} ${year}`
+  })
+
+  const vipData = Array.from({ length: monthsBetween + 1 }, () => Math.floor(Math.random() * 1000))
+  const guestsData = Array.from({ length: monthsBetween + 1 }, () => Math.floor(Math.random() * 1000))
+
+  return {
+    labels,
+    datasets: [
+      {
+        maxBarThickness: 45,
+        label: 'VIP Member',
+        backgroundColor: warning,
+        borderColor: 'transparent',
+        data: vipData
+      },
+      {
+        maxBarThickness: 45,
+        backgroundColor: info,
+        label: 'Guests',
+        borderColor: 'transparent',
+        data: guestsData
+      }
+    ]
+  }
+}
+
 const VipAndGuestsData = (props: VerticalBarProps) => {
-  const { info, warning, labelColor, borderColor, legendColor } = props
+  const { info, warning, labelColor, borderColor } = props
+  const { getVIPandGuestChart } = DashboardService()
+
+  const [fromDate, setFromDate] = useState<string | undefined>()
+  const [toDate, setToDate] = useState<string | undefined>()
+
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
+
+  useQuery({
+    queryKey: [`VIPandGuest`, fromDate, toDate],
+    queryFn: () =>
+      getVIPandGuestChart({
+        data: {
+          from: fromDate,
+          to: toDate
+        }
+      })
+  })
+
+  const updateDates = (from: Date, to: Date) => {
+    setFromDate(from.toISOString().slice(0, 10))
+    setToDate(to.toISOString().slice(0, 10))
+  }
+
+  useEffect(() => {
+    updateDates(startDate, endDate)
+  }, [startDate, endDate])
+
+  const [chartData, setChartData] = useState<ChartData<'bar'>>(generateChartData(startDate, endDate, warning, info))
 
   const options: ChartOptions<'bar'> = {
     indexAxis: 'x',
@@ -50,39 +125,80 @@ const VipAndGuestsData = (props: VerticalBarProps) => {
       legend: {
         align: 'end',
         position: 'top',
-        labels: { color: legendColor }
+        labels: { color: labelColor }
       }
     }
   }
 
-  const data: ChartData<'bar'> = {
-    labels: ['January', 'February', 'March ', 'April', 'May'],
-    datasets: [
-      {
-        maxBarThickness: 45,
-        label: 'VIP Member',
-        backgroundColor: warning,
-        borderColor: 'transparent',
-        data: [710, 350, 580, 460, 120]
-      },
-      {
-        maxBarThickness: 45,
-        backgroundColor: info,
-        label: 'Guests',
-        borderColor: 'transparent',
-        data: [430, 590, 510, 240, 360]
-      }
-    ]
-  }
-
   return (
-    <Card>
-      <CardHeader title='VIP Members and Guests Data' />
-      <CardContent>
-        <Bar data={data} height={400} options={options} />
-      </CardContent>
-    </Card>
+    <DatePickerWrapper>
+      <Card>
+        <Box sx={styles.headerWrapper}>
+          <CardHeader title='VIP Members and Guests Data' />
+          <Box sx={styles.datePicker}>
+            <Box>
+              <DatePicker
+                customInput={<PickersComponent customwidth='100%' label='Select Start Date' />}
+                selected={startDate}
+                onChange={date => {
+                  if (date) {
+                    setStartDate(date)
+                    setChartData(generateChartData(date, endDate, warning, info))
+                  }
+                }}
+              />
+            </Box>
+            <Box>
+              <DatePicker
+                customInput={<PickersComponent customwidth='100%' label='Select End Date' />}
+                selected={endDate}
+                onChange={date => {
+                  if (date) {
+                    setEndDate(date)
+                    setChartData(generateChartData(startDate, date, warning, info))
+                  }
+                }}
+              />
+            </Box>
+          </Box>
+        </Box>
+        <CardContent>
+          <Bar data={chartData} height={400} options={options} />
+        </CardContent>
+      </Card>
+    </DatePickerWrapper>
   )
+}
+
+const styles = {
+  headerWrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    p: 2,
+    flexDirection: {
+      xs: 'column',
+      sm: 'column',
+      md: 'column',
+      lg: 'row'
+    }
+  },
+  datePicker: {
+    display: 'flex',
+    width: {
+      xs: '100%',
+      sm: '100%',
+      md: '100%',
+      lg: '50%'
+    },
+    flexDirection: {
+      xs: 'column',
+      sm: 'column',
+      md: 'column',
+      lg: 'row'
+    },
+    gap: 3
+  }
 }
 
 export default VipAndGuestsData
