e88dd91e7713440f0ce15bd423b593dd56cc8c55
VincentPaul <oo.vincentpaul@1bit.com.ph>
2023-05-09 14:34:45 +0800

    Integrated Sentry in GoldCoinBundles and Modal

--- src/pages/bundles/components/GoldCoinBundleModal.tsx
+++ src/pages/bundles/components/GoldCoinBundleModal.tsx
@@ -1,207 +1,281 @@
-import Typography from '@mui/material/Typography';
+// ** React Imports
 import React, { useState } from 'react'
-import Stack, { StackProps } from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Switch from '@mui/material/Switch';
-import TextField from '@mui/material/TextField';
-import CircularProgress from '@mui/material/CircularProgress';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import BundlesService from '@/services/api/BundlesService';
-import { useTranslateString } from '@/utils/TranslateString';
-import { Box, Menu, MenuItem } from '@mui/material';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
+// ** Next Imports
+import { useRouter } from 'next/router'
+
+// ** MUI Imports
+import { Box, Button, CircularProgress, Menu, MenuItem, Switch, TextField, Typography } from '@mui/material'
+import Stack, { StackProps } from '@mui/material/Stack'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+
+// ** Utils Imports
+import { useTranslateString } from '@/utils/TranslateString'
+
+// ** TanStack Imports
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+// ** Hooks/Services Imports
+import BundlesService from '@/services/api/BundlesService'
+import { captureError } from '@/services/Sentry'
+
+// optional props to be passed if editing a bundle instead
 type Props = {
-
-	// optional props to be passed if editing a bundle instead
-	isEditingGoldCoinBundle?: boolean
-	bundleID?: string
-	site_id?: number
-	uniqueSites?: any
-	bundleName?: string
-	bundlePrice?: string
-	bundleDescription?: string
-	isBundleOn?: boolean
-	onClose: () => void
+  isEditingGoldCoinBundle?: boolean
+  bundleID?: string
+  site_id?: number
+  uniqueSites?: any
+  bundleName?: string
+  bundlePrice?: string
+  bundleDescription?: string
+  isBundleOn?: boolean
+  onClose: () => void
 }
 
 const GoldCoinBundleModal = (props: Props) => {
-	const [bundleName, setBundleName] = useState(props.bundleName ?? "")
-	const [bundlePrice, setBundlePrice] = useState<any>(props.bundlePrice ?? "")
-	const [bundleDescription, setBundleDescription] = useState(props.bundleDescription ?? "")
-	const [isBundleActive, setIsBundleActive] = useState(props.isBundleOn ?? false)
-	const [selectedSiteID, setSelectedSiteID] = useState(0)
+  const router = useRouter()
+  const currentLocation = router.asPath
 
-	// MENU
-	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-	const isMenuOpen = Boolean(anchorEl);
-	const openMenu = (event: React.MouseEvent<HTMLButtonElement>) => setAnchorEl(event.currentTarget)
-	const closeMenu = () => setAnchorEl(null)
+  const [bundleName, setBundleName] = useState(props.bundleName ?? '')
+  const [bundlePrice, setBundlePrice] = useState<any>(props.bundlePrice ?? '')
+  const [bundleDescription, setBundleDescription] = useState(props.bundleDescription ?? '')
+  const [isBundleActive, setIsBundleActive] = useState(props.isBundleOn ?? false)
+  const [selectedSiteID, setSelectedSiteID] = useState(0)
 
-	const validateInputs = () => {
-		if (bundleName === "") return false
-		if (bundlePrice === "" || bundlePrice < 200) return false
-		if (bundleDescription === "") return false
+  // MENU
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const isMenuOpen = Boolean(anchorEl)
+  const openMenu = (event: React.MouseEvent<HTMLButtonElement>) => setAnchorEl(event.currentTarget)
+  const closeMenu = () => setAnchorEl(null)
 
-		return true
-	}
+  const validateInputs = () => {
+    if (bundleName === '') return false
+    if (bundlePrice === '' || bundlePrice < 200) return false
+    if (bundleDescription === '') return false
 
-	// Get QueryClient from the context
-	const queryClient = useQueryClient();
-	const { addCoinsBundle, editCoinsBundle } = BundlesService();
+    return true
+  }
 
-	const { mutate: mutateAddNewCoinBundle, isLoading: addedLoading, isSuccess: addedSuccess } = useMutation(addCoinsBundle, {
-		onSuccess: (data) => {
-			console.log(data);
-			queryClient.invalidateQueries({
-				queryKey: ["allCoinsBundle"],
-			});
-			props.onClose()
-		},
-		onError: (error) => {
-			alert(error);
-		},
-	});
+  // Get QueryClient from the context
+  const queryClient = useQueryClient()
+  const { addCoinsBundle, editCoinsBundle } = BundlesService()
 
-	const { mutate: mutateEditCoinBundle, isLoading: editingLoading, isSuccess: editingSuccess } = useMutation(editCoinsBundle, {
-		onSuccess: (data) => {
-			console.log(data);
-			queryClient.invalidateQueries({
-				queryKey: ["allCoinsBundle"],
-			});
-			props.onClose()
-		},
-		onError: (error) => {
-			alert(error);
-		},
-	});
+  const {
+    mutate: mutateAddNewCoinBundle,
+    isLoading: addedLoading,
+    isSuccess: addedSuccess
+  } = useMutation(addCoinsBundle, {
+    onSuccess: data => {
+      console.log(data)
+      queryClient.invalidateQueries({
+        queryKey: ['allCoinsBundle']
+      })
+      props.onClose()
+    },
+    onError: (e: any) => {
+      const {
+        data: { error }
+      } = e
+      for (const key in error) {
+        error[key].forEach((value: any) => {
+          captureError(currentLocation, `${value}, addCoinsBundle() GoldCoinBundleModal`)
+        })
+      }
+    }
+  })
 
-	const editGoldCoinBundle = () => {
-		if (validateInputs()) {
-			mutateEditCoinBundle({
-				bundle_id: props.bundleID,
-				data: {
-					site_id: selectedSiteID,
-					name: bundleName,
-					price: Number(bundlePrice),
-					amount: Number(bundlePrice),
-					description: bundleDescription,
-					active: isBundleActive,
-				},
-			});
-		}
-	}
+  const {
+    mutate: mutateEditCoinBundle,
+    isLoading: editingLoading,
+    isSuccess: editingSuccess
+  } = useMutation(editCoinsBundle, {
+    onSuccess: data => {
+      console.log(data)
+      queryClient.invalidateQueries({
+        queryKey: ['allCoinsBundle']
+      })
+      props.onClose()
+    },
+    onError: (e: any) => {
+      const {
+        data: { error }
+      } = e
+      for (const key in error) {
+        error[key].forEach((value: any) => {
+          captureError(currentLocation, `${value}, editCoinsBundle() GoldCoinBundleModal`)
+        })
+      }
+    }
+  })
 
-	const addGoldCoinBundle = () => {
-		if (validateInputs()) {
-			mutateAddNewCoinBundle({
-				data: {
-					site_id: selectedSiteID,
-					name: bundleName,
-					price: Number(bundlePrice),
-					amount: Number(bundlePrice),
-					description: bundleDescription,
-					active: isBundleActive,
-				},
-			});
-		}
-	}
+  const editGoldCoinBundle = () => {
+    if (validateInputs()) {
+      mutateEditCoinBundle({
+        bundle_id: props.bundleID,
+        data: {
+          site_id: selectedSiteID,
+          name: bundleName,
+          price: Number(bundlePrice),
+          amount: Number(bundlePrice),
+          description: bundleDescription,
+          active: isBundleActive
+        }
+      })
+    }
+  }
 
-	const isBeingAddedOrEdited = (addedLoading || addedSuccess) || (editingLoading || editingSuccess)
-	const loadingStyle = isBeingAddedOrEdited ? { opacity: 0.5, cursor: "not-allowed" } : null
-	
-	const TranslateString = useTranslateString()
-	
-	return (
-		<Stack {...modalContainer}>
-			<Typography variant="h5" color="white" textAlign="center" my={2} mb={4}>{props.isEditingGoldCoinBundle ? TranslateString("Edit") : TranslateString("Add")} {TranslateString("Gold Coin Bundle")}</Typography>
-			<Stack gap={2} width={300} sx={loadingStyle}>
-				{props.isEditingGoldCoinBundle ? null : 
-					<Box>
-						<Button variant="contained" color="primary" onClick={openMenu} endIcon={<KeyboardArrowDownIcon />}>Site ID: {selectedSiteID}</Button>
-						<Menu anchorEl={anchorEl} open={isMenuOpen} onClose={closeMenu}>
-							<MenuItem onClick={() => { setSelectedSiteID(0); closeMenu() }}>0: Default</MenuItem>
-							{props.uniqueSites.map((item: any) => <MenuItem key={item?.id} onClick={() => { setSelectedSiteID(item?.id); closeMenu() }}>{item?.id}: {item?.name}</MenuItem>)}
-						</Menu>
-					</Box>
-				}
-				{isBeingAddedOrEdited ? <CircularProgress sx={loaderStyle} /> : null}
-				<Stack {...cardContainer}>
-					<Stack flexDirection="row" alignItems="center" justifyContent="space-between">
-						<TextField
-							error={props.isEditingGoldCoinBundle ? bundleName === "" : false}
-							sx={textFieldStyle}
-							value={bundleName}
-							onChange={(event) => setBundleName(event.target.value)}
-							label={TranslateString("Bundle Name")} />
-						<Switch checked={isBundleActive} onChange={event => { setIsBundleActive(event.target.checked)}} />
-					</Stack>
-					<TextField
-						type="number"
-						error={props.isEditingGoldCoinBundle ? bundlePrice < 200 || bundlePrice === "" : false}
-						sx={textFieldStyle}
-						inputProps={{ min: 0 }}
-						value={bundlePrice}
-						onChange={(event) => setBundlePrice(event.target.value)}
-						label={TranslateString("Gold Coin")} />
-					{props.isEditingGoldCoinBundle && bundlePrice < 200 ? <Typography variant="caption" color="error">must be atleast 200</Typography> : undefined}
-				</Stack>
-				<Stack {...cardContainer}>
-					<TextField
-						error={props.isEditingGoldCoinBundle ? bundleDescription === "" : false}
-						sx={textFieldStyle}
-						value={bundleDescription}
-						onChange={(event) => setBundleDescription(event.target.value)}
-						label={TranslateString("Description")}
-						multiline
-						rows={4} />
-				</Stack>
-				<Stack flexDirection="row" gap={2} mt={4}>
-					<Button variant="contained" color="error" onClick={props.onClose} fullWidth>{TranslateString("Cancel")}</Button>
-					<Button variant="contained" color="success" onClick={props.isEditingGoldCoinBundle ? editGoldCoinBundle : addGoldCoinBundle} fullWidth>{TranslateString("Save")}</Button>
-				</Stack>
-			</Stack>
-		</Stack>
-	)
+  const addGoldCoinBundle = () => {
+    if (validateInputs()) {
+      mutateAddNewCoinBundle({
+        data: {
+          site_id: selectedSiteID,
+          name: bundleName,
+          price: Number(bundlePrice),
+          amount: Number(bundlePrice),
+          description: bundleDescription,
+          active: isBundleActive
+        }
+      })
+    }
+  }
+
+  const isBeingAddedOrEdited = addedLoading || addedSuccess || editingLoading || editingSuccess
+  const loadingStyle = isBeingAddedOrEdited ? { opacity: 0.5, cursor: 'not-allowed' } : null
+
+  const TranslateString = useTranslateString()
+
+  return (
+    <Stack {...modalContainer}>
+      <Typography variant='h5' color='white' textAlign='center' my={2} mb={4}>
+        {props.isEditingGoldCoinBundle ? TranslateString('Edit') : TranslateString('Add')}{' '}
+        {TranslateString('Gold Coin Bundle')}
+      </Typography>
+      <Stack gap={2} width={300} sx={loadingStyle}>
+        {props.isEditingGoldCoinBundle ? null : (
+          <Box>
+            <Button variant='contained' color='primary' onClick={openMenu} endIcon={<KeyboardArrowDownIcon />}>
+              Site ID: {selectedSiteID}
+            </Button>
+            <Menu anchorEl={anchorEl} open={isMenuOpen} onClose={closeMenu}>
+              <MenuItem
+                onClick={() => {
+                  setSelectedSiteID(0)
+                  closeMenu()
+                }}
+              >
+                0: Default
+              </MenuItem>
+              {props.uniqueSites.map((item: any) => (
+                <MenuItem
+                  key={item?.id}
+                  onClick={() => {
+                    setSelectedSiteID(item?.id)
+                    closeMenu()
+                  }}
+                >
+                  {item?.id}: {item?.name}
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        )}
+        {isBeingAddedOrEdited ? <CircularProgress sx={loaderStyle} /> : null}
+        <Stack {...cardContainer}>
+          <Stack flexDirection='row' alignItems='center' justifyContent='space-between'>
+            <TextField
+              error={props.isEditingGoldCoinBundle ? bundleName === '' : false}
+              sx={textFieldStyle}
+              value={bundleName}
+              onChange={event => setBundleName(event.target.value)}
+              label={TranslateString('Bundle Name')}
+            />
+            <Switch
+              checked={isBundleActive}
+              onChange={event => {
+                setIsBundleActive(event.target.checked)
+              }}
+            />
+          </Stack>
+          <TextField
+            type='number'
+            error={props.isEditingGoldCoinBundle ? bundlePrice < 200 || bundlePrice === '' : false}
+            sx={textFieldStyle}
+            inputProps={{ min: 0 }}
+            value={bundlePrice}
+            onChange={event => setBundlePrice(event.target.value)}
+            label={TranslateString('Gold Coin')}
+          />
+          {props.isEditingGoldCoinBundle && bundlePrice < 200 ? (
+            <Typography variant='caption' color='error'>
+              must be atleast 200
+            </Typography>
+          ) : undefined}
+        </Stack>
+        <Stack {...cardContainer}>
+          <TextField
+            error={props.isEditingGoldCoinBundle ? bundleDescription === '' : false}
+            sx={textFieldStyle}
+            value={bundleDescription}
+            onChange={event => setBundleDescription(event.target.value)}
+            label={TranslateString('Description')}
+            multiline
+            rows={4}
+          />
+        </Stack>
+        <Stack flexDirection='row' gap={2} mt={4}>
+          <Button variant='contained' color='error' onClick={props.onClose} fullWidth>
+            {TranslateString('Cancel')}
+          </Button>
+          <Button
+            variant='contained'
+            color='success'
+            onClick={props.isEditingGoldCoinBundle ? editGoldCoinBundle : addGoldCoinBundle}
+            fullWidth
+          >
+            {TranslateString('Save')}
+          </Button>
+        </Stack>
+      </Stack>
+    </Stack>
+  )
 }
 
 export default GoldCoinBundleModal
 
 const modalContainer: StackProps = {
-	sx: {
-		backgroundColor: theme => theme.customBflyColors.alwaysPrimary,
-		transform: "translate(-50%, -50%)",
-	},
-	position: "absolute",
-	top: "50%",
-	left: "50%",
-	p: 4,
-	borderRadius: 1,
+  sx: {
+    backgroundColor: theme => theme.customBflyColors.alwaysPrimary,
+    transform: 'translate(-50%, -50%)'
+  },
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  p: 4,
+  borderRadius: 1
 }
 
 const cardContainer: StackProps = {
-	bgcolor: "#D9D9D9",
-	borderRadius: 0.5,
-	p: 2,
-	gap: 2,
+  bgcolor: '#D9D9D9',
+  borderRadius: 0.5,
+  p: 2,
+  gap: 2
 }
 
 const textFieldStyle = {
-	backgroundColor: "white",
-	borderRadius: 1,
-	'& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button': {
-		"WebkitAppearance": "none",
-		"margin": 0
-	},
+  backgroundColor: 'white',
+  borderRadius: 1,
+  '& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button': {
+    WebkitAppearance: 'none',
+    margin: 0
+  }
 }
 
-const loaderStyle =
-{
-	position: "absolute",
-	top: 0,
-	bottom: 0,
-	left: 0,
-	right: 0,
-	margin: "auto",
-	zIndex: 1,
+const loaderStyle = {
+  position: 'absolute',
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  margin: 'auto',
+  zIndex: 1
 }
--- src/pages/bundles/pages/GoldCoinBundlesPage.tsx
+++ src/pages/bundles/pages/GoldCoinBundlesPage.tsx
@@ -1,79 +1,158 @@
+// ** React Imports
 import React, { useState } from 'react'
-import GoldCoinBundleItem from '../components/GoldCoinBundleItem';
-import GoldCoinBundleModal from '../components/GoldCoinBundleModal';
-import { useQuery } from '@tanstack/react-query';
-import BundlesService from '@/services/api/BundlesService';
-import SitesService from '@/services/api/SitesService';
-import { useTranslateString }  from '@/utils/TranslateString';
-import { Box, Menu, MenuItem, Grid, Modal, Stack, Typography, Button, CircularProgress } from '@mui/material';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+// ** Next Imports
+import { useRouter } from 'next/router'
+
+// ** MUI Imports
+import { Box, Menu, MenuItem, Grid, Modal, Stack, Typography, Button, CircularProgress } from '@mui/material'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+
+// ** Project/Other Imports
+import GoldCoinBundleItem from '../components/GoldCoinBundleItem'
+import GoldCoinBundleModal from '../components/GoldCoinBundleModal'
+
+// ** Utils Imports
+import { useTranslateString } from '@/utils/TranslateString'
+
+// ** TanStack Imports
+import { useQuery } from '@tanstack/react-query'
+
+// ** Hooks/Services Imports
+import BundlesService from '@/services/api/BundlesService'
+import SitesService from '@/services/api/SitesService'
+import { captureError } from '@/services/Sentry'
 
 const GoldCoinBundlesPage = () => {
+  const router = useRouter()
+  const currentLocation = router.asPath
+
   const [open, setOpen] = useState(false)
   const handleClose = () => setOpen(false)
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
-  const openMenu = (event: React.MouseEvent<HTMLButtonElement>) =>  setAnchorEl(event.currentTarget)
+  const openMenu = (event: React.MouseEvent<HTMLButtonElement>) => setAnchorEl(event.currentTarget)
   const closeMenu = () => setAnchorEl(null)
   const isMenuOpen = Boolean(anchorEl)
   const [selectedSiteID, setSelectedSiteID] = useState<any>(undefined)
   const [uniqueSites, setUniqueSites] = useState<any>([])
   const TranslateString = useTranslateString()
-  
+
   // FETCH ALL GOLD COIN BUNDLES
   const { getAllCoinsBundle } = BundlesService()
   const { isLoading, data } = useQuery({
-    queryKey: ["allCoinsBundle", selectedSiteID],
-    queryFn: () => getAllCoinsBundle({
-      data: { 
-        site_id: selectedSiteID,
-        with: "sites",
-        exclude: selectedSiteID === undefined ? 0 : undefined
+    queryKey: ['allCoinsBundle', selectedSiteID],
+    queryFn: () =>
+      getAllCoinsBundle({
+        data: {
+          site_id: selectedSiteID,
+          with: 'sites',
+          exclude: selectedSiteID === undefined ? 0 : undefined
+        }
+      }),
+    onSuccess: data => {
+      console.log('COINS BUNDLE:', data?.data)
+    },
+    onError: (e: any) => {
+      const {
+        data: { error }
+      } = e
+      for (const key in error) {
+        error[key].forEach((value: any) => {
+          captureError(currentLocation, `${value}, getAllCoinsBundle() GoldCoinBundlesPage`)
+        })
       }
-    }),
-    onSuccess: (data) => { console.log("COINS BUNDLE:", data?.data) },
-    onError: (error) => { console.log(error) }
+    }
   })
 
   // AS GOD USER, WE FETCH THE UNIQUE SITE IDs
   const { getSitesList } = SitesService()
-  const { } = useQuery({
-    queryKey: ["allSitesList"],
+  const {} = useQuery({
+    queryKey: ['allSitesList'],
     queryFn: () => getSitesList({}),
-    onSuccess: (data) => { console.log("SITES LIST:", data?.data); setUniqueSites(data?.data) },
-    onError: (error) => { console.log(error) }
+    onSuccess: data => {
+      console.log('SITES LIST:', data?.data)
+      setUniqueSites(data?.data)
+    },
+    onError: (e: any) => {
+      const {
+        data: { error }
+      } = e
+      for (const key in error) {
+        error[key].forEach((value: any) => {
+          captureError(currentLocation, `${value}, getSitesList() GoldCoinBundlesPage`)
+        })
+      }
+    }
   })
 
   return (
     <>
-      <Stack sx={{ padding: { xs: 4, sm: 8 }}} bgcolor={theme => theme.customBflyColors.alwaysPrimary} borderRadius={1}>
-        <Stack justifyContent={{ xs: "center", sm: "space-between" }} flexDirection={{ xs: "column", sm: "row" }} alignItems="center" gap={2} mb={4}>
-          <Box display="flex" gap={10} alignItems="center">
-            <Typography variant="h5" component="div" color="white">{TranslateString("Gold Coin Bundle")}</Typography>
-            <Button variant="contained" onClick={openMenu} endIcon={<KeyboardArrowDownIcon />}>
-              {selectedSiteID === 0 ? "Default" : null}
-              {selectedSiteID === undefined ? "All Sites" : null}
+      <Stack
+        sx={{ padding: { xs: 4, sm: 8 } }}
+        bgcolor={theme => theme.customBflyColors.alwaysPrimary}
+        borderRadius={1}
+      >
+        <Stack
+          justifyContent={{ xs: 'center', sm: 'space-between' }}
+          flexDirection={{ xs: 'column', sm: 'row' }}
+          alignItems='center'
+          gap={2}
+          mb={4}
+        >
+          <Box display='flex' gap={10} alignItems='center'>
+            <Typography variant='h5' component='div' color='white'>
+              {TranslateString('Gold Coin Bundle')}
+            </Typography>
+            <Button variant='contained' onClick={openMenu} endIcon={<KeyboardArrowDownIcon />}>
+              {selectedSiteID === 0 ? 'Default' : null}
+              {selectedSiteID === undefined ? 'All Sites' : null}
               {uniqueSites.find((item: any) => item.id === selectedSiteID)?.name}
             </Button>
             <Menu anchorEl={anchorEl} open={isMenuOpen} onClose={closeMenu}>
-              <MenuItem onClick={() => { setSelectedSiteID(undefined); closeMenu() }}>All Sites</MenuItem>
-              <MenuItem onClick={() => { setSelectedSiteID(0); closeMenu() }}>0: Default</MenuItem>
-              {uniqueSites.map((item: any) =>
-                <MenuItem key={item} onClick={() => { setSelectedSiteID(item?.id); closeMenu() }}>{item?.id}: {item?.name}</MenuItem>
-              )}
+              <MenuItem
+                onClick={() => {
+                  setSelectedSiteID(undefined)
+                  closeMenu()
+                }}
+              >
+                All Sites
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  setSelectedSiteID(0)
+                  closeMenu()
+                }}
+              >
+                0: Default
+              </MenuItem>
+              {uniqueSites.map((item: any) => (
+                <MenuItem
+                  key={item}
+                  onClick={() => {
+                    setSelectedSiteID(item?.id)
+                    closeMenu()
+                  }}
+                >
+                  {item?.id}: {item?.name}
+                </MenuItem>
+              ))}
             </Menu>
           </Box>
-          <Button variant="contained" onClick={() => setOpen(true)}>{TranslateString("Create Gold Coin Bundle")}</Button>
+          <Button variant='contained' onClick={() => setOpen(true)}>
+            {TranslateString('Create Gold Coin Bundle')}
+          </Button>
         </Stack>
-        {isLoading ?
-          <Stack my={16} alignItems="center">
+        {isLoading ? (
+          <Stack my={16} alignItems='center'>
             <CircularProgress />
           </Stack>
-          :
+        ) : (
           <Grid
             container
             gap={10.5}
-            justifyContent={{ xs: "center", sm: data?.data?.length % 4 === 0 ? "space-between" : "flex-start" }}>
-            {data?.data?.map((item: any) =>
+            justifyContent={{ xs: 'center', sm: data?.data?.length % 4 === 0 ? 'space-between' : 'flex-start' }}
+          >
+            {data?.data?.map((item: any) => (
               <GoldCoinBundleItem
                 key={item._id}
                 bundleID={item._id}
@@ -82,12 +161,12 @@ const GoldCoinBundlesPage = () => {
                 bundlePrice={item.price}
                 bundleDescription={item.description}
                 isBundleOn={item.active}
-                siteName={item.sites  === null ? "Default" : item.sites.name}
+                siteName={item.sites === null ? 'Default' : item.sites.name}
               />
-            )}
+            ))}
           </Grid>
-        }
-        </Stack>
+        )}
+      </Stack>
       {/* TO BE GLOBALIZED LATER SO WE DONT NEED TO RENDER THIS HERE */}
       {/* CREATING GCB MODAL */}
       <Modal open={open} onClose={handleClose}>
