7e039f9f23547de52c65790a1141ca801454d839
dexi-1bit <dexi@1bit.com.ph>
2023-05-09 15:16:29 +0800

    Fix for feed features missing components

--- src/pages/settings/components/feeds/index.tsx
+++ src/pages/settings/components/feeds/index.tsx
@@ -1,213 +1,231 @@
-// ** React Imports
+// ** React Imporst
 import React from 'react'
 
 // ** MUI Imports
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
+import CircularProgress from '@mui/material/CircularProgress'
 
 // ** Step Components
-import AllStory from '@/pages/studio/newsfeed/views/AllStory'
-import AllPhoto from '@/pages/studio/newsfeed/views/AllPhoto'
-import AllVideo from '@/pages/studio/newsfeed/views/AllVideo'
-import VideosWithPhotos from '@/pages/studio/newsfeed/views/VideosWithPhotos'
+import FeedList from '@/pages/studio/newsfeed/views/FeedList'
 import TextField from '@mui/material/TextField'
 import InputAdornment from '@mui/material/InputAdornment'
 
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
+
+// ** API Imports
+import { useQuery, useInfiniteQuery, useQueryClient } from '@tanstack/react-query'
+import FeedsService from '@/services/api/FeedsService'
+
+// ** Utils
+import createSkeleton from '@/utils/createSkeleton'
 import { useTranslateString } from '@/utils/TranslateString';
+import Translations from '@/layouts/components/Translations'
+import EditNewsFeedDrawer from '@/pages/studio/newsfeed/views/EditNewsFeedDrawer'
 
-// ** Style Imports
+// ** Types
+import { IFeedStory } from '@/context/types'
+interface IFeedButton {
+  title : string
+  param : {
+    [key : string] : boolean
+  }
+}
 
-const steps = [
-    {
-        title: 'All Story Feeds',
-    },
-    {
-        title: 'All Photo Feeds',
-    },
-    {
-        title: 'All Video Feeds',
-    },
-    {
-        title: 'Videos With Photos',
-    },
+const feedButtons : IFeedButton[] = [
+  {
+    title: 'All Story Feeds',
+    param : { story_feeds_only: true }
+  },
+  {
+    title: 'All Photo Feeds',
+    param : { images_only: true }
+  },
+  {
+    title: 'All Video Feeds',
+    param : { video_only: true }
+  },
+  {
+    title: 'Videos With Photos',
+    param : { video_images: true }
+  }
 ]
 
-interface FeedsProps {
-    onClose: () => void
-}
 
-const SelectFeaturedFeeds: React.FC<FeedsProps> = ({ onClose }) => {
+type Props = {}
 
-    // ** States
-    const [activeTab, setActiveTab] = React.useState<number>(0)
+// ** Feeds Params
+const defaultParams = { with: 'user', page: 1, approval : 'Approved' }
 
-    // TURN THIS TO ENUM SO ITS READABLE
-    const getActiveTabContent = (step: number) => {
-        switch (step) {
-            case 0:
-                return <AllStory />
-            case 1:
-                return <AllPhoto />
-            case 2:
-                return <AllVideo />
-            case 3:
-                return <VideosWithPhotos />
-            default:
-                return null
-        }
-    }
+const SelectFeaturedFeeds = (props: Props) => {
+  // ** States
+  const [activeTab, setActiveTab] = React.useState<number>(0)
+  const [feedParams, setFeedParams] = React.useState<{}>({ ...defaultParams, story_feeds_only : true })
 
-    const renderContent = () => {
-        return getActiveTabContent(activeTab)
-    }
+  // ** Drawer states
+  const [ open, setOpen ] = React.useState<boolean>(false)
+  const [feedRow, setFeedRow] = React.useState<IFeedStory | null>(null)
 
-    const handleBtnClick = () => {
-        onClose();
-    }
-    
-    const TranslateString = useTranslateString()
-
-    return (
-        <Box sx={styles.container}>
-            <Box sx={styles.topButtons}>
-                <Box sx={styles.inputSave}>
-                    <TextField sx={styles.title} label={TranslateString("Featured Feed Title")} />
-                    <Button
-                        sx={styles.saveBtn}
-                        onClick={handleBtnClick}
-                    >
-                        {TranslateString("Save")}
-                    </Button>
-                </Box>
-                <Box sx={styles.steps}>
-                    {steps.map((step, index) => {
-                        return (
-                            <Button
-                                sx={{
-                                    ...styles.stepsBtn,
-                                    backgroundColor: index === activeTab ? '#9747FF' : '#FFF',
-                                    color: index === activeTab ? '#FFF' : '#000',
-                                }}
-                                key={index}
-                                size='medium'
-                                onClick={() => { setActiveTab(index) }} >{TranslateString(step.title)}
-                            </Button>
-                        )
-                    })}
-                    <TextField
-                        sx={styles.search}
-                        variant='outlined'
-                        placeholder={TranslateString("Search")}
-                        InputProps={{
-                            endAdornment: (
-                                <InputAdornment position='end'>
-                                    <Icon icon='mdi:microphone' />
-                                </InputAdornment>
-                            )
-                        }} />
-                </Box>
-            </Box>
-            <Box sx={styles.content}>
-                {renderContent()}
-            </Box>
-        </Box>
-    )
-}
-
-const styles = {
-  container: {
-    marginInline: 'auto',
-    marginTop: '2rem',
-    paddingBottom: '4rem',
-    alignItems: 'center'
-  },
-  topButtons: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    flexDirection: 'column',
-    marginBottom: '2rem'
-  },
-  inputSave: {
-    display: 'flex',
-    flexDirection: {
-      xs: 'column',
-      sm: 'column',
-      md: 'row',
-      lg: 'row'
-    },
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    mb: 5,
-    gap: {
-      xs: 5,
-      lg: 0
-    }
-  },
-  title: {
-    width: {
-      xs: '100%',
-      lg: '30%'
-    }
-  },
-  saveBtn: {
-    border: '1px solid black',
-    width: {
-      xs: '100%',
-      sm: '100%',
-      md: '50%',
-      lg: 300
-    },
-    color: '#000',
-    textTransform: 'uppercase',
-    backgroundColor: '#FFF',
-    '&:hover': {
-      backgroundColor: '#7B0BB0',
-      color: '#FFF'
-    }
-  },
-  steps: {
-    display: 'flex',
-    flexDirection: {
-      xs: 'column',
-      sm: 'column',
-      md: 'column',
-      lg: 'row'
-    },
-    justifyContent: 'space-between',
-    gap: {
-      xs: 3,
-      lg: '2.5rem'
-    },
-    marginBottom: '0rem'
-  },
-  stepsBtn: {
-    border: '1px solid black',
-    width: {
-      xs: '100%',
-      lg: 150
-    },
-    textTransform: 'uppercase',
-    fontSize: 10,
-    '&:hover': {
-      backgroundColor: '#7B0BB0',
-      color: '#FFF'
-    },
-  },
-  search: {
-    '& input': {
-      padding: '.5em 1em',
-    },
-    '& fieldset': {
-      borderRadius: '0 !important',
-      padding: '.5em 1em',
-    }
-  },
-  content: {
-    borderRadius: '5px',
-    maxWidth: '100%'
+  const toggleEditDrawer = () => {
+    setOpen(!open)
   }
+
+  // ** QueryAPI
+  const { getFeeds } = FeedsService()
+  const { isLoading, data, fetchNextPage, hasNextPage, isFetchingNextPage, isRefetching } = useInfiniteQuery({
+    queryKey: ['getFeeds', feedParams],
+    getNextPageParam: (prevData: any) => {
+      // TBR any
+      let nextData = prevData.next_page_url
+      if (nextData == undefined) {
+        return undefined
+      }
+      let searchParams = new URLSearchParams(nextData.split('?')[1]) // extracts the query string and creates a URLSearchParams object
+      let page = searchParams.get('page') // retrieves the value of the 'page' parameter
+      let myObj = feedParams
+      let nextParams = { ...myObj, page: page }
+      return nextParams
+    },
+    queryFn: ({ pageParam = { ...feedParams } }) => {
+      return getFeeds(pageParam)
+    }
+  })
+
+  // ** Hooks
+  React.useEffect(() => {
+    console.log('changed tab so I will call refetch from react query')
+  }, [activeTab])
+
+  const handleFeedParams = (index: number) => {
+
+    const feedParams = { ...feedButtons[index].param, ...defaultParams }
+    setFeedParams( feedParams )
+    setActiveTab(index)
+  }
+
+  const handleFeedItemClick = (feed : IFeedStory) => {
+    console.log('@@@@@@@@@ THE FEED ID', feed)
+    setFeedRow(feed)
+    setOpen(true)
+  }
+
+  const getActiveTabContent = (step: number) => {
+    if (data) {
+      let flatMapDataArray = data.pages.flatMap(data => [data.data])
+      let flatMap = flatMapDataArray.flatMap(data => [...data])
+      return <FeedList data={flatMap} handleFeedItemClick={handleFeedItemClick} />
+    }
+  }
+
+  const renderContent = () => {
+    return getActiveTabContent(activeTab)
+  }
+
+  const TranslateString = useTranslateString()
+
+  return (
+    <Box sx={{ marginInline: 'auto', marginTop: '2rem', paddingBottom: '4rem', alignItems: 'center' }}>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: ['center', 'space-between'],
+          flexDirection: ['row'],
+          alignItems: ['center', 'initial'],
+          gap: ['0rem', '2rem'],
+          marginBottom: '2rem'
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: ['column', 'row'],
+            gap: ['1rem', '2.5rem'],
+            marginBottom: '0rem'
+          }}
+        >
+          {feedButtons.map((button, index) => {
+            return (
+              <Button
+                key={index}
+                sx={{ paddingBlock: '.5em', textTransform: 'uppercase' }}
+                size='medium'
+                onClick={() => {
+                  handleFeedParams(index)
+                }}
+                variant={index == activeTab ? 'contained' : 'outlined'}
+              >
+                {TranslateString(button.title)}
+              </Button>
+            )
+          })}
+        </Box>
+        <Box>
+          <TextField
+            sx={{
+              display: 'none', //search box
+              '& input': {
+                padding: '.5em 1em'
+              },
+              '& fieldset': {
+                borderRadius: '0 !important',
+                padding: '.5em 1em'
+              }
+            }}
+            variant='outlined'
+            placeholder='Search'
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position='end'>
+                  <Icon icon='mdi:microphone' />
+                </InputAdornment>
+              )
+            }}
+          />
+        </Box>
+      </Box>
+
+      <Box sx={{ borderRadius: '5px', maxWidth: '90%', marginInline: 'auto', padding: '1em' }}>
+        <Box sx={{ display: 'flex', columnGap: '1rem', marginBottom: '1.5rem' }}>
+          {/* <Button onClick={handleInvalidate} variant='contained'>SEARCH BY CONTENT CREATOR</Button>
+          <Button variant='contained'>SORT BY DATE</Button> */}
+        </Box>
+        {isLoading && (
+          <Box sx={{ display: 'flex' }}>
+            <Grid container spacing={10}>
+              {createSkeleton(2)}
+            </Grid>
+          </Box>
+        )}
+        {data && renderContent()}
+
+        {hasNextPage && (
+          <Box>
+            <Grid container spacing={10}>
+              <Grid mt={15} display='flex' justifyContent='center' alignItems='center' textAlign='center' item xs={12}>
+                <Button
+                  disabled={isFetchingNextPage ? true : false}
+                  variant='contained'
+                  onClick={() => {
+                    fetchNextPage()
+                  }}
+                >
+                  {isFetchingNextPage ? <CircularProgress size={15} color='secondary' /> : <Translations text="Load More Stories"/>}
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        )}
+      </Box>
+      
+      {
+        feedRow && 
+        <EditNewsFeedDrawer open={open} row={feedRow as IFeedStory} toggle={ () => toggleEditDrawer() } />
+      }
+      
+    </Box>
+  )
 }
 
 export default SelectFeaturedFeeds
