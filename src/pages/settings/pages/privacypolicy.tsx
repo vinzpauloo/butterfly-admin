19f882272870794031630f7622ba21b318a32d67
VincentPaul <oo.vincentpaul@1bit.com.ph>
2023-05-09 13:47:25 +0800

    Integrated Sentry for Privacy Policy and TOS

--- src/pages/settings/pages/privacypolicy.tsx
+++ src/pages/settings/pages/privacypolicy.tsx
@@ -1,23 +1,57 @@
+// ** React Imports
 import React, { useState } from 'react'
-import { ContentState, EditorState, convertFromHTML } from 'draft-js';
-import { useQuery } from '@tanstack/react-query';
-import SitesService from '@/services/api/SitesService';
-import EditorContainer from '../components/forms/EditorContainer';
+
+// ** Next Imports
+import { useRouter } from 'next/router'
+
+// ** Third Party Imports
+import { ContentState, EditorState, convertFromHTML } from 'draft-js'
+
+// ** Project/Other Imports
+import EditorContainer from '../components/forms/EditorContainer'
+
+// ** TanStack Imports
+import { useQuery } from '@tanstack/react-query'
+
+// ** Hooks/Services Imports
+import { captureError } from '@/services/Sentry'
+import SitesService from '@/services/api/SitesService'
 
 const PrivacyPolicy = () => {
-  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+  const router = useRouter()
+  const currentLocation = router.asPath
+
+  const [editorState, setEditorState] = useState(() => EditorState.createEmpty())
 
   const { getSiteOtherDetails } = SitesService()
   const { isLoading } = useQuery({
-    queryKey: ["privacyPolicy"],
-    queryFn: () => getSiteOtherDetails({ data: { detail: "policy" } }),
-    onSuccess: (data) => {
-      setEditorState(EditorState.createWithContent(ContentState.createFromBlockArray(convertFromHTML(data).contentBlocks)))
+    queryKey: ['privacyPolicy'],
+    queryFn: () => getSiteOtherDetails({ data: { detail: 'policy' } }),
+    onSuccess: data => {
+      setEditorState(
+        EditorState.createWithContent(ContentState.createFromBlockArray(convertFromHTML(data).contentBlocks))
+      )
     },
-    onError: (error) => { console.log(error) }
+    onError: (e: any) => {
+      const {
+        data: { error }
+      } = e
+      for (const key in error) {
+        error[key].forEach((value: any) => {
+          captureError(currentLocation, `${value}, getSiteOtherDetails() Privacy Policy`)
+        })
+      }
+    }
   })
 
-  return <EditorContainer title="Privacy Policy" editorState={editorState} setEditorState={setEditorState} isLoading={isLoading} />
+  return (
+    <EditorContainer
+      title='Privacy Policy'
+      editorState={editorState}
+      setEditorState={setEditorState}
+      isLoading={isLoading}
+    />
+  )
 }
 
 export default PrivacyPolicy
--- src/pages/settings/pages/tos.tsx
+++ src/pages/settings/pages/tos.tsx
@@ -1,23 +1,57 @@
+// ** React Imports
 import React, { useState } from 'react'
-import { ContentState, EditorState, convertFromHTML } from 'draft-js';
-import { useQuery } from '@tanstack/react-query';
-import SitesService from '@/services/api/SitesService';
-import EditorContainer from '../components/forms/EditorContainer';
+
+// ** Next Imports
+import { useRouter } from 'next/router'
+
+// ** Third Party Imports
+import { ContentState, EditorState, convertFromHTML } from 'draft-js'
+
+// ** Project/Other Imports
+import EditorContainer from '../components/forms/EditorContainer'
+
+// ** TanStack Imports
+import { useQuery } from '@tanstack/react-query'
+
+// ** Hooks/Services Imports
+import SitesService from '@/services/api/SitesService'
+import { captureError } from '@/services/Sentry'
 
 const TermsOfService = () => {
-  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+  const router = useRouter()
+  const currentLocation = router.asPath
+
+  const [editorState, setEditorState] = useState(() => EditorState.createEmpty())
 
   const { getSiteOtherDetails } = SitesService()
   const { isLoading } = useQuery({
-    queryKey: ["termsOfService"],
-    queryFn: () => getSiteOtherDetails({ data: { detail: "provisions" } }),
-    onSuccess: (data) => {
-      setEditorState(EditorState.createWithContent(ContentState.createFromBlockArray(convertFromHTML(data).contentBlocks)))
+    queryKey: ['termsOfService'],
+    queryFn: () => getSiteOtherDetails({ data: { detail: 'provisions' } }),
+    onSuccess: data => {
+      setEditorState(
+        EditorState.createWithContent(ContentState.createFromBlockArray(convertFromHTML(data).contentBlocks))
+      )
     },
-    onError: (error) => { console.log(error) }
+    onError: (e: any) => {
+      const {
+        data: { error }
+      } = e
+      for (const key in error) {
+        error[key].forEach((value: any) => {
+          captureError(currentLocation, `${value}, getSiteOtherDetails() Terms Of Service`)
+        })
+      }
+    }
   })
 
-  return <EditorContainer title="Terms of Service" editorState={editorState} setEditorState={setEditorState} isLoading={isLoading} />
+  return (
+    <EditorContainer
+      title='Terms of Service'
+      editorState={editorState}
+      setEditorState={setEditorState}
+      isLoading={isLoading}
+    />
+  )
 }
 
 export default TermsOfService
