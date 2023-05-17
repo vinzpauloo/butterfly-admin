8a190e081ff64c53b53869fa9c350dd8e172fb27
Liyan Anajao <123532430+LiyanAnajao1Bit@users.noreply.github.com>
2023-05-17 18:11:30 +0800

    Update FQDNS as super agent (WIP)
    - can update and create new FQDNS

--- src/pages/fqdn/components/LinksContainer.tsx
+++ src/pages/fqdn/components/LinksContainer.tsx
@@ -1,67 +1,22 @@
 import { Box, Button, Typography, Card, CardContent, CircularProgress, Stack } from '@mui/material'
-import React, { useState } from 'react'
-import FQDNService from '@/services/api/FQDNService'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { useAuth } from '@/services/useAuth'
+import React from 'react'
 import NewFQDNLinkInput from './NewFQDNLinkInput'
 import ExistingFQDNLinkInput from './ExistingFQDNLinkInput'
-
-type response = {
-  id: number
-  site_id: number
-  name: string
-  type: 'Api' | 'Streaming' | 'Photo'
-}
 
 type Props = {
   type: 'Api' | 'Streaming' | 'Photo'
   isLoading: boolean
-  data: response[]
+  data: string[]
+  dataSetter: React.Dispatch<React.SetStateAction<string[]>>
+  newData: string[]
+  newDataSetter: React.Dispatch<React.SetStateAction<string[]>>
 }
 
 const LinksContainer = (props: Props) => {
-  const { type, isLoading: getLoading, data } = props
-  const [newInputs, setNewInputs] = useState<string[]>([])
-  const auth = useAuth()
+  const { type, isLoading, data, dataSetter, newData, newDataSetter } = props
 
   const addMoreTextField = () => {
-    setNewInputs([...newInputs, ''])
-  }
-
-  const { addFQDN } = FQDNService()
-  const queryClient = useQueryClient()
-  const { mutate: mutateAddFQDN, isLoading: addLoading } = useMutation(addFQDN, {
-    onSuccess: data => {
-      console.log(data)      
-      queryClient.invalidateQueries({ queryKey: ['fqdns'] })
-      setNewInputs([])
-    },
-    onError: error => { console.log(error) },
-  },)
-
-  const isValidUrl = (string: string) => {
-    try { 
-      new URL(string)
-
-      return true
-    } catch (err) {
-      return false
-    }
-  }
-
-  const addNewFQDNS = () => {
-    // create a new array of validated FQDNs
-    const newFQDNSArray: any = newInputs.map((item: string) => {
-      if (isValidUrl(item)) return { name: item, type: type }
-      else return null
-    })
-
-    mutateAddFQDN({
-      data: {
-        site: auth?.user?.site,
-        fqdns: newFQDNSArray
-      }
-    })
+    newDataSetter([...newData, ''])
   }
 
   return (
@@ -69,20 +24,17 @@ const LinksContainer = (props: Props) => {
       <CardContent>
         <Typography variant='h6' component='h2' align='center' mb={4} textTransform='uppercase'>{type}</Typography>
         <Typography variant='subtitle2' color='primary' align='left' fontWeight={500} mb={4} textTransform='capitalize'>minimum 3 links</Typography>
-        <Stack gap={4} position='relative' py={getLoading ? 12 : undefined}>
-          {getLoading && <CircularProgress sx={{position:'absolute', margin: 'auto', top:0, left: 0, right: 0, bottom: 0}} /> }
-          {data.map((item: response, index: number) => 
-            <ExistingFQDNLinkInput key={item?.id} name={item?.name} id={item?.id} index={index} type={type} />           
+        <Stack gap={4} position='relative' py={isLoading ? 12 : undefined}>
+          {isLoading && <CircularProgress sx={{position:'absolute', margin: 'auto', top:0, left: 0, right: 0, bottom: 0}} /> }
+          {data.map((item: string, index: number) => 
+            item !== null && <ExistingFQDNLinkInput key={index} name={item} index={index} data={data} dataSetter={dataSetter} />           
           )}
-          {newInputs.map((item: string, index: number) =>
-            <NewFQDNLinkInput key={index} newInputs={newInputs} index={index} setNewInputs={setNewInputs} isLoading={addLoading} />
+          {newData.map((item: string, index: number) =>
+            item !== null && <NewFQDNLinkInput key={index} newInputs={newData} index={index} setNewInputs={newDataSetter} />
           )}
-          {!getLoading &&
+          {!isLoading &&
             <Box display='flex' justifyContent='center' gap={6}>
-              <Button disabled={getLoading} variant='contained' color='info' onClick={addMoreTextField}>Add More</Button>
-              {newInputs.length > 0 && 
-                <Button disabled={getLoading} type='submit' variant='contained' color='primary' onClick={addNewFQDNS}>Save</Button>
-              }
+              <Button disabled={isLoading} variant='contained' color='info' onClick={addMoreTextField}>Add More</Button>
             </Box>
           }
         </Stack>
